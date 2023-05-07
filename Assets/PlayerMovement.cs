bb24f125b9bf2d3af227f0c166ee160fa121552f
Jonathan Carey <jonathan.carey1995@gmail.com>
2023-05-07 18:06:35 -0400

    added idle/running animations

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,18 +4,67 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    private Animator anim;
+    private SpriteRenderer sprite;
+
+    private float dirX;
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        Debug.Log("Look, a frog!"); 
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        float dirX = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(dirX * 7f, rb.velocity.y);
+
+        if (Input.GetButtonDown("Jump"))
         {
-            GetComponent<Rigidbody2D>().velocity = new Vector3(0, 14, 0);
+            rb.velocity = new Vector3(rb.velocity.x, 14f);
+            Debug.ClearDeveloperConsole();
+        }
+
+
+
+        //added this but now the animation for running is being called
+        //UpdateAnimationUpdate();
+        if (dirX > 0f)
+        {
+            anim.SetBool("running", true);
+            sprite.flipX = false;
+        }
+        else if (dirX < 0f)
+        {
+            anim.SetBool("running", true);
+            sprite.flipX = true;
+        }
+        else
+        {
+            anim.SetBool("running", false);
         }
     }
+
+    //private void UpdateAnimationUpdate()
+    //{
+    //    if (dirX > 0f)
+    //    {
+    //        anim.SetBool("running", true);
+    //        sprite.flipX = false;
+    //    }
+    //    else if (dirX < 0f)
+    //    {
+    //        anim.SetBool("running", true);
+    //        sprite.flipX = true;
+    //    }
+    //    else
+    //    {
+    //        anim.SetBool("running", false);
+    //    }
+    //}
 }
